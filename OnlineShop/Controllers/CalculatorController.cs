c1d47e55423d0dd8d304439fff3656e448cfc7f9
Валентин Ветюков <finik001@gmail.com>
2025-04-05 15:36:49 +0300

    Complete lesson 4-2-3 Additional.

--- OnlineShop/Controllers/CalculatorController.cs
+++ OnlineShop/Controllers/CalculatorController.cs
@@ -6,14 +6,18 @@ public class CalculatorController : Controller
 {
     public string Index(int a, int b, string method)
     {
+        var tempLik = "https://localhost:7224/calculator/index/10/5/+";
+        
         switch (method)
         {
+            case "+":
+                return $"{a} + {b} = {a + b}";
             case "-":
                 return $"{a} - {b} = {a - b}";
             case "*":
                 return $"{a} * {b} = {a * b}";
         }
         
-        return $"{a} + {b} = {a + b}";
+        return "Вы ввели не корректный URL, введите URL в следующем формате, " + tempLik;
     }
 }
