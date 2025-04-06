6d3c6c5b0e033830d14f474b7d16c0d903e59d3e
Валентин Ветюков <finik001@gmail.com>
2025-04-06 13:02:15 +0300

    Move data in ServiceRepository and create new Controller Service.

--- OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/Controllers/HomeController.cs
@@ -7,53 +7,22 @@ namespace OnlineShop.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
-    private readonly List<Service> _services = new()
-    {
-        new Service
-        {
-            Id = new Guid("ecd4480b-235c-4a91-9b88-64787c3748bf"), 
-            Name = "Service 01", 
-            Cost = 100.50, 
-            Description = "Descr - 001"
-        },
-        new Service
-        {
-            Id = new Guid("8703089e-0e0b-4c58-9c33-30419aa7b56e"), 
-            Name = "Service 02", 
-            Cost = 200.45, 
-            Description = "Descr - 002"
-        },
-        new Service
-        {
-            Id = new Guid("05acd97a-1fe2-4168-bd15-43daa8a620b6"), 
-            Name = "Service 03", 
-            Cost = 300.55, 
-            Description = "Descr - 003"
-        },
-    };
+    private readonly ServiceRepository _serviceRepository;
 
     public HomeController(ILogger<HomeController> logger)
     {
         _logger = logger;
+        _serviceRepository = new ServiceRepository();
     }
 
     public string Index(Guid? id)
     {
         var result = "";
 
-        if (id == null)
+        foreach (var service in _serviceRepository.GetList())
         {
-            foreach (var service in _services)
-            {
-                result += service;
-                result += "\r\n\r\n";
-            }
-        }
-        else
-        {
-            var item = _services.FirstOrDefault(el => el.Id == id);
-            result += item == null ? "Элемент не найден" : item + $"\r\n{item.Description}";
+            result += service;
+            result += "\n\n";
         }
         
         return result;
